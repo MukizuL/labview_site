9d0a2bc6d81e7abad1b4f4a9d3cab92175571e2f
MukizuL <53896777+MukizuL@users.noreply.github.com>
2023-05-30 19:58:44 +0700

    commented unused items

--- src/components/Module2.js
+++ src/components/Module2.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './Module2.css';
+import React from "react";
+import "../App.css";
+//import { Button } from './Button';
+import "./Module2.css";
 
 function Module2() {
-  return (
-    <div className='Module2-main-container'>
-     
-    </div>
-  );
+  return <div className="Module2-main-container"></div>;
 }
 
 export default Module2;
--- src/components/Module3.js
+++ src/components/Module3.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './Module3.css';
+import React from "react";
+import "../App.css";
+//import { Button } from './Button';
+import "./Module3.css";
 
 function Module3() {
-  return (
-    <div className='Module3-main-container'>
-     
-    </div>
-  );
+  return <div className="Module3-main-container"></div>;
 }
 
 export default Module3;
