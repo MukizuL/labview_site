0c5f257335d280f0a7c3ef603d6c3d35e66e03d9
MukizuL <53896777+MukizuL@users.noreply.github.com>
2023-05-30 18:23:43 +0700

    upgraded Button, filled Overview

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './Main.css';
-
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./Main.css";
 
 function Main() {
   return (
-    <div className='Main-main-container'>
+    <div className="Main-main-container">
       <h1>LabView</h1>
       <p>Образовательная платформа</p>
-      <div className='Main-main-btns'>
-        <Button 
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+      <div className="Main-main-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+          destination="/overview"
         >
           НАЧАТЬ
         </Button>
